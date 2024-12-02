973f85afada1a27c3d84797c70cc2d310990378f
BebekJK <kevinjk2802@gmail.com>
2024-12-03 06:39:41 +0800

    add input fetcher function

---
+++ utils.py
@@ -0,0 +1,12 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+session = os.getenv('SESSION')
+day = 2
+
+if __name__ == '__main__':
+    data = requests.get(f'https://adventofcode.com/2024/day/{day}/input', cookies={'session': session}).text
+    with open(f'input/day{day}.txt', 'w') as f:
+        f.write(data)
