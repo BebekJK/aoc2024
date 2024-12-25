cc717af0eb30fdcccd1eba74731ef5f022234a83
BebekJK <kevinjk2802@gmail.com>
2024-12-25 19:20:58 +0700

    day 24 and day 25 solution

--- solution/day24.py
+++ solution/day24.py
@@ -1,23 +1,67 @@
+import random
+
+nodes = {}
+
 def split_line(line):
+    if line == '':
+        return None
+    
+    if ':' in line:
+        line = line.split(':')
+        return line[0], 1 if line[1].strip() == '1' else 0
+    
     line = line.split(' ')
-    line = [int(i) for i in line]
-    return line
+    return line[0], line[1], line[2], line[4]
 
 # Part One
 def solve_part_one():
     with open('input/day24.txt') as f:
         data = f.read().splitlines()
         data = [split_line(line) for line in data]
+        data = [line for line in data if line]
         
-        # print(f"Answer for part one: {ans}")
+        num_node = 90
+        node_data, adj_data = data[:num_node], data[num_node:]
+        words = {}
+        
+        for line in node_data:
+            nodes[line[0]] = line[1]
+            words[line[0]] = str(line[1])
+            
+        count = 0
+        visited = [0 for _ in range(len(adj_data))]
+        
+        while count < len(adj_data):
+            for i, adj in enumerate(adj_data):
+                if visited[i]:
+                    continue
+                
+                if adj[0] in nodes and adj[2] in nodes:
+                    visited[i] = 1
+                    count += 1
+                    
+                    if adj[1] == 'AND':
+                        nodes[adj[3]] = nodes[adj[0]] & nodes[adj[2]]
+                    elif adj[1] == 'OR':
+                        nodes[adj[3]] = nodes[adj[0]] | nodes[adj[2]]
+                    elif adj[1] == 'XOR':
+                        nodes[adj[3]] = nodes[adj[0]] ^ nodes[adj[2]]
+                    
+                    words[adj[3]] = f'({words[adj[0]]} {adj[1]} {words[adj[2]]})'
+        
+        ans = sorted(list(nodes.keys()))
+        ans = [nodes[key] for key in ans if key[0] == 'z']
+        ans = sum([2**i for i, val in enumerate(ans) if val])
+        
+        # for key, val in words.items():
+        #     if key[0] == 'z':
+        #         print(f"{key}: {val}")  
+        print(f"Answer for part one: {ans}")
         
 # Part Two
 def solve_part_two():
-    with open('input/day24.txt') as f:
-        data = f.read().splitlines()
-        data = [split_line(line) for line in data]
-        
-        # print(f"Answer for part two: {ans}")
+    ## idk ?
+    pass
 
 if __name__ == '__main__':
     solve_part_one()
--- solution/day25.py
+++ solution/day25.py
@@ -1,15 +1,37 @@
-def split_line(line):
-    line = line.split(' ')
-    line = [int(i) for i in line]
-    return line
-
 # Part One
 def solve_part_one():
+    def enc_lock(lock):
+        l = lock[1:]
+        l = [[c == '#' for c in r] for r in l]
+        l = [sum([l[r][c] for r in range(6)]) for c in range(5)]
+        return l
+    
+    def enc_key(key):
+        k = key[:-1]
+        k = [[c == '#' for c in r] for r in k]
+        k = [sum([k[r][c] for r in range(6)]) for c in range(5)]
+        return k
+    
     with open('input/day25.txt') as f:
         data = f.read().splitlines()
-        data = [split_line(line) for line in data]
+        data = [line for line in data if line]
         
-        # print(f"Answer for part one: {ans}")
+        data = [data[7*i:7*(i+1)] for i in range(len(data)//7)]
+        locks = [line for line in data if line[0] == '#####']
+        keys = [line for line in data if line[-1] == '#####']
+        
+        
+        ans = 0
+        
+        for lock in locks:
+            l = enc_lock(lock)
+            for key in keys:
+                k = enc_key(key)
+                s = sum([l[i]+k[i] < 6 for i in range(5)])
+                ans += (s == 5)
+        
+        
+        print(f"Answer for part one: {ans}")
         
 # Part Two
 def solve_part_two():
@@ -21,4 +43,4 @@ def solve_part_two():
 
 if __name__ == '__main__':
     solve_part_one()
-    solve_part_two()
+    # solve_part_two()
